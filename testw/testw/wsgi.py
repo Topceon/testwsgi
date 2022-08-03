b8f62dbc722f41660ec9bae6559cfe9e4d58627b
Inox Danil <inoxdanil@gmail.com>
2022-08-03 13:45:39 +0600

    sys path

--- testw/testw/wsgi.py
+++ testw/testw/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+sys.path.insert(0, 'app/testwsgi/testw/testw')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testw.settings')
